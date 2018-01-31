25199c28a4e3cfa8a7ccbb619bf46cadbd11d7a0
drbro <drbro@LAPTOP-70FTRIAC>
2018-01-31 12:46:31 -0500

    Finished assignment

--- GettingStarted/src/com/darren/dbrock/Main.java
+++ GettingStarted/src/com/darren/dbrock/Main.java
@@ -8,18 +8,23 @@ public class Main {
 	/**
 	 * 4 Pillars:
 	 * 
-	 * 	Abstraction-
+	 * 	Abstraction- An inheritance hierarchy, where the higher classes are simpler or more general, and define more general 
+	 *               and abstract implementation. While the lower classes in the hierarchy are more concrete and define more 
+	 *               detailed implementations.
 	 * 
-	 * 	Encapsulation-
+	 * 	Encapsulation- To hide details of implementation, limit what other classes have access to.
 	 * 
-	 * 	Inheritance-
+	 * 	Inheritance- When an object or class is based on another object, but uses the same implementation.
 	 * 
-	 * 	Polymorphism-
+	 * 	Polymorphism- The ability to present the same interface for different underlying forms.
 	 */
 	
 	public static List<Computer> computers = new ArrayList<Computer>();
 	
 	public static void main(String[] args) {
+		new Desktop();
+		new Laptop();
+		
 		for(Computer comp : computers) {
 			System.out.println(comp.toString());
 		}
