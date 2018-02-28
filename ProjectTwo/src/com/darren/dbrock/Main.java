02681ffd32a202bfd8a9bfcb6752195cff3990b9
drbro <drbro@LAPTOP-70FTRIAC>
2018-02-28 15:49:39 -0500

    added notes

--- ProjectTwo/src/com/darren/dbrock/Main.java
+++ ProjectTwo/src/com/darren/dbrock/Main.java
@@ -2,4 +2,12 @@ package com.darren.dbrock;
 
 public class Main {
  
+	/**
+	 * 1. Load my exams
+	 * 2. Offer user the available exam choices
+	 * 3. Give exam
+	 *   a. Ask questions
+	 *   b. Collect responses
+	 * 4. Calculate score
+	 */
 }
