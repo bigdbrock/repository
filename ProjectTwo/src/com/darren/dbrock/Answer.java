ea79b51ecacc6025f37b4086743e02317286daee
drbro <drbro@LAPTOP-70FTRIAC>
2018-03-07 22:47:18 -0500

    updates

--- ProjectTwo/src/com/darren/dbrock/Answer.java
+++ ProjectTwo/src/com/darren/dbrock/Answer.java
@@ -3,6 +3,9 @@ package com.darren.dbrock;
 public class Answer {
 	
 	private String answer;
+	private String answer2;
+	private String answer3;
+	private String answer4;
 	private int answerNumber;
 	
 	public Answer(int answerNumber, String answer) {
@@ -10,8 +13,41 @@ public class Answer {
 		this.answerNumber = answerNumber;
 	}
 	
+	public Answer(int answerNumber, String answer, String answer2) {
+		this.answerNumber = answerNumber;
+		this.answer = answer;
+		this.answer2 = answer2;
+	}
+	
+	public Answer(int answerNumber, String answer, String answer2, String answer3) {
+		this.answerNumber = answerNumber;
+		this.answer = answer;
+		this.answer2 = answer2;
+		this.answer3 = answer3;
+	}
+	
+	public Answer(int answerNumber, String answer, String answer2, String answer3, String answer4) {
+		this.answerNumber = answerNumber;
+		this.answer = answer;
+		this.answer2 = answer2;
+		this.answer3 = answer3;
+		this.answer4 = answer4;
+	}
+	
 	public String getAnswer() {
 		return answer;
+	}
+	
+	public String getAnswer2() {
+		return answer2;
+	}
+	
+	public String getAnswer3() {
+		return answer3;
+	}
+	
+	public String getAnswer4() {
+		return answer4;
 	}
 	
 	public int getAnswerNumber() {
--- ProjectTwo/src/com/darren/dbrock/MultipleChoice.java
+++ ProjectTwo/src/com/darren/dbrock/MultipleChoice.java
@@ -1,55 +1,98 @@
 package com.darren.dbrock;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MultipleChoice implements Exam {
 
+	private List<Question> questions = new ArrayList<Question>();
+	private List<Answer> answers = new ArrayList<Answer>();
+
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "MultipleChoice";
 	}
-
 	@Override
 	public void startExam() {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public List<Question> getQuestions() {
-		// TODO Auto-generated method stub
-		return null;
+		return questions;
 	}
 
-	@Override
-	public List<Answer> getAnswers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	//pull from file
+	 /** Array [0] = exam name
+	 *   Array [1] = question number
+	 *   Array [2] = question string*/
 	@Override
 	public void loadQuestions() {
-		// TODO Auto-generated method stub
-
+		try {
+			FileReader fr = new FileReader("Questions");
+			@SuppressWarnings("resource")
+			BufferedReader br = new BufferedReader(fr);
+			
+			String currentLine = "";
+			String examName;
+			String questionNumber;
+			String question;
+			while((currentLine = br.readLine()) != null) {
+				String[] line = currentLine.split(":");
+				examName = line[0];
+				questionNumber = line[1];
+				question = line[2];
+				if(examName.toLowerCase().equals(getName().toLowerCase())) {
+					questions.add(new Question(Integer.parseInt(questionNumber), question));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	//pull from file
 	@Override
 	public void loadAnswers() {
-		// TODO Auto-generated method stub
-
+		try {
+			FileReader fr = new FileReader("Answers");
+			@SuppressWarnings("resource")
+			BufferedReader br = new BufferedReader(fr);
+			
+			String currentLine = "";
+			String examName;
+			String answerNumber;
+			String answer;
+			while((currentLine = br.readLine()) != null) {
+				String[] line = currentLine.split(":");
+				examName = line[0];
+				answerNumber = line[1];
+				answer = line[2];
+				if(examName.toLowerCase().equals(getName().toLowerCase())) {
+					answers.add(new Answer(Integer.parseInt(answerNumber), answer));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public float calculateScore() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
+	//write to a file
 	@Override
 	public void writeReportCard() {
-		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public List<Answer> getAnswers() {
+		return answers;
 	}
 
 }
